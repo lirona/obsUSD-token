9f4471d81f9358bdd529e5df9ce3643da9c2b19d
Ron Turetzky <turetzkyron@gmail.com>
2024-11-07 06:05:40 +0700

    chore: adding deploy script

---
+++ script/DeployBuildersDollar.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {Script} from "forge-std/Script.sol";
+import {BuildersDollar} from "../src/BuildersDollar.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {EIP173ProxyWithReceive} from "../src/vendor/EIP173ProxyWithReceive.sol";
+contract DeployBuildersDollar is Script {
+    using stdJson for string;
+
+    function run() external {
+        // Load config
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/script/config/optimism/deploy-config.json");
+        string memory json = vm.readFile(path);
+        
+        // Parse config
+        address dai = json.readAddress(".dai");
+        address aDai = json.readAddress(".aDai");
+        address aavePool = json.readAddress(".aavePool");
+        address aaveRewards = json.readAddress(".aaveRewards");
+        string memory name = json.readString(".name");
+        string memory symbol = json.readString(".symbol");
+
+        // Deploy
+        vm.broadcast();
+        BuildersDollar dollar = new BuildersDollar(
+            dai,
+            aDai,
+            aavePool,
+            aaveRewards
+        );
+        EIP173ProxyWithReceive proxy = new EIP173ProxyWithReceive(
+            address(dollar),
+            address(this),
+            abi.encodeWithSelector(dollar.initialize.selector, name, symbol)    
+        );
+
+        // Initialize
+
+    }
+} 
